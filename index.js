f1107cf10a4354d7b6f1ec434e2c517594963a6f
BreTre <bctrepal@gmail.com>
2024-04-28 15:49:17 -0400

    created classes and input svg elements

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-const shapes = require('./lib/shapes');
-const generateSVG = require('./lib/generateSVG');
-const fs = require('fs');
-const inquirer = require('inquirer');
+const CLI = require('./lib/cli.js');
 
-const prompts = [
-        {
-            type: "input",
-            message: "Enter up to three characters:",
-            name: "text"
-        },
-        {
-            type: "input",
-            message: "Enter a color keyword or hexidecimal number for the text color:",
-            name: "tcolor"
-        },
-        {
-            type: "list",
-            message: "Choose a shape:",
-            name: "shape",
-            choices: ["circle", "triangle", "square"]
-        },
-        {
-            type: "input",
-            message: "Enter a color keyword or hexidecimal number for the shape color:",
-            name: "scolor"
-        }
-    ];
+const cli = new CLI();
 
-    function writeToFile(data) {
-        const svgContent = generateSVG(data);
-
-        fs.writeFile('logo.svg', svgContent, (err) => 
-        err ? console.log(err) : console.log('logo.svg generated.'))
-    }
-
-    function init() {
-        inquirer.prompt(prompts)
-        .then((data) => {
-            writeToFile(data);
-        })
-    }
-
-    init();
+cli.run();
